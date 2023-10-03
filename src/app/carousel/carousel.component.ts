4ced09455de4cbbd78edbe19d6f4c6388def77fc
victorlbitten <victorlbittenc@gmail.com>
2023-10-03 17:57:13 -0300

    Basicão do carrossel tá rolante

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,15 +1,56 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent implements OnInit{
+export class CarouselComponent implements AfterViewInit, OnInit {
+  @ViewChild('carousel') carousel: ElementRef;
+
+  @Input() imageWidth: number;
+  _fallbackImageWidthInPx = 400;
   @Input() images: string[] = [];
+
   currentIndex = 0;
+  carouselTranslationInPx = 0;
+  imageWidthInPx: number;
+
+  constructor (
+    private renderer: Renderer2
+  ) {}
+
 
   ngOnInit(): void {
-    
+    this.imageWidthInPx = this.imageWidth || this._fallbackImageWidthInPx;
+  }
+
+  ngAfterViewInit(): void {
+    this.renderer.setStyle(this.carousel.nativeElement, 'width', `${this.imageWidthInPx}px`);
+    this.renderer.setStyle(this.carousel.nativeElement, 'height', `${this.imageWidthInPx/1.666}px`);
+
+  }
+
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex < this.images.length - 1)
+      ? this.currentIndex + 1
+      : 0;
+    this.translateContainer();
+  }
+
+  previousImage() {
+    this.currentIndex = (this.currentIndex > 0)
+      ? this.currentIndex - 1
+      : this.images.length - 1;
+    this.translateContainer();
+  }
+
+  goToImageByIndex (index: number) {
+
+  }
+
+  translateContainer() {
+    this.carouselTranslationInPx = - (this.currentIndex * this.imageWidthInPx);
   }
 }
--- src/app/texts.const.ts
+++ src/app/texts.const.ts
@@ -6,6 +6,12 @@ export const capabilitiesText = [
       'Embora as medidas de geometria da via geralmente exijam um sistema de inspeção dedicado separado, o LRAIL é capaz de realizar essas tarefas simultaneamente com outras tarefas importantes, como inspeção de grampos e pregos, inspeção de lastro, inspeção de travessas e outras.',
       'O LRAIL utiliza inteligência artificial para primeiro corrigir digitalizações 3D de acordo com o movimento do veículo de medição usando sensores IMUs embutidos. Em seguida, os dados 3D são analisados para detectar trilhos, localizar o topo dos trilhos para medidas de nível transversal e o ponto de medida da bitola (5/8" abaixo do topo do trilho) para medidas de bitola e alinhamento dos trilhos. Os comprimentos de corda são personalizáveis para corresponder ao padrão específico do cliente.',
     ],
+    images: [
+      'assets/images/rail_geometry/1.png',
+      'assets/images/rail_geometry/2.png',
+      'assets/images/rail_geometry/3.png',
+      'assets/images/rail_geometry/4.png',
+    ]
   },
   {
     title: 'Inspeção de Desgaste de trilhos',
@@ -15,6 +21,12 @@ export const capabilitiesText = [
       'Os algoritmos do LRAIL analisam dados em 3D para realizar medições de desgaste nos trilhos superiores, laterais e nos cantos. Essas medições de desgaste podem ser aplicadas tanto a trilhos de perfil padrão quanto aos trilhos ranhurados, como os usados em sistemas de metrô.',
       'Além disso, por meio dos algoritmos de detecção de alterações do LRAIL, as variações no desgaste podem ser identificadas de forma automática e relatadas, destacando às equipes  de manutenção da via as áreas da rede ferroviária que requerem maior atenção.',
     ],
+    images: [
+      'assets/images/rail_wear_inspection/1.png',
+      'assets/images/rail_wear_inspection/2.png',
+      'assets/images/rail_wear_inspection/3.png',
+      'assets/images/rail_wear_inspection/4.png',
+    ]
   },
   {
     title: 'Inspeção de Grampos',
@@ -25,6 +37,12 @@ export const capabilitiesText = [
       'O LRAIL simplifica a tarefa de inspeção de grampos, utilizando inteligência artificial para analisar tanto dados em 2D quanto em 3D, com o objetivo de detectar com maior precisão grampos presentes e ausentes (e seus componentes), tipo de grampo, grampos soltos, grampos danificados e grampos cobertos.',
       'Além disso, por meio dos algoritmos de Detecção de Mudanças do LRAIL, é possível identificar automaticamente alterações no inventário e nas condições dos grampos e relatar essas mudanças, direcionando as equipes de manutenção da via para as partes da rede que mais precisam de atenção.',
     ],
+    images: [
+      'assets/images/clip_inspection/1.png',
+      'assets/images/clip_inspection/2.png',
+      'assets/images/clip_inspection/3.png',
+      'assets/images/clip_inspection/4.png',
+    ]
   },
   {
     title: ' Inspeção de Nível de Lastro e Contaminação',
@@ -35,6 +53,13 @@ export const capabilitiesText = [
       'No entanto, a inspeção do lastro é frequentemente um processo subjetivo devido à falta deuma definição formal para quando o lastro é considerado contaminado e devido ao processomuitas vezes manual de inspeção.',
       'O LRAIL simplifica a tarefa de inspeção do lastro ao utilizar inteligência artificialpara analisar dados em 2D e 3D, permitindo medir de forma precisa e objetiva a altura do lastroe a contaminação do lastro. A altura do lastro é medida automaticamente nas extremidades dosdormentes, e áreas com altura inadequada (conforme definido pelo usuário) podem ser identificadas.A contaminação do lastro, tanto no lado da bitola quanto no lado do campo, pode ser detectada equantificada automaticamente e de forma objetiva. Mudanças na altura do lastro e no nível decontaminação entre inspeções subsequentes também podem ser detectadas e relatadas automaticamente.',
     ],
+    images: [
+      'assets/images/ballast_level/1.png',
+      'assets/images/ballast_level/2.png',
+      'assets/images/ballast_level/3.png',
+      'assets/images/ballast_level/4.png',
+      'assets/images/ballast_level/5.png',
+    ]
   },
   {
     title: 'Inspeção de Dormentes (Travessas)',
@@ -45,6 +70,13 @@ export const capabilitiesText = [
       'O LRAIL simplifica a tarefa de inspeção de travessas ao utilizar a inteligência artificial para analisar dados em 2D e 3D, permitindo a detecção e o relatório mais precisos da quantidade de travessas, de sua posição, do material, da cobertura superficial, do desalinhamento, bem como da qualidade das travessas de madeira e de concreto.',
       'Além disso, por meio dos algoritmos de Detecção de Mudanças do LRAIL, é possível identificar automaticamente e relatar mudanças na qualidade das travessas e em seu desalinhamento entre inspeções subsequentes, direcionando as equipes de manutenção da via para as partes da rede que mais precisam de atenção.',
     ],
+    images: [
+      'assets/images/sleeper_inspection/1.png',
+      'assets/images/sleeper_inspection/2.png',
+      'assets/images/sleeper_inspection/3.png',
+      'assets/images/sleeper_inspection/4.png',
+      'assets/images/sleeper_inspection/5.png',
+    ]
   },
   {
     title: 'Detecção de Mudanças',
@@ -55,6 +87,11 @@ export const capabilitiesText = [
       'A inteligência artificial do LRAIL alinha automaticamente as inspeções repetidas e, em seguida,analisa dados em 2D e 3D para detectar mudanças entre eles.',
       'Mudanças positivas, devido à realização de manutenção, assim como mudanças negativas podemser detectadas. Exemplos de mudanças que podem ser detectadas incluem:',
     ],
+    images: [
+      'assets/images/change_detection/1.png',
+      'assets/images/change_detection/2.png',
+      'assets/images/change_detection/3.png'
+    ]
   }
 ];
 
@@ -71,6 +108,12 @@ export const aboutText = {
     "O LRAIL proporciona maior ROI(retorno de investimento) ao fornecer uma inspeção multifuncional que fornece imagens 2D e digitalização 3D em uma única passagem com análise totalmente automatizada usando Inteligência Artificial.",
     "O LRAIL captura uma impressionante resolução de varredura de 1 mm X e Y e 0, 1 mm Z dos trilhos, dormentes e área de lastro a velocidades de até 120 km / h.",
     "Os algoritmos de Inteligência Artificial do LRAIL medem e detectam automaticamente alterações relacionadas à bitola, nível cruzado, alinhamento, pontas, clipes, placas de amarração, folga de junta, aparafusamento de barra de junta, desgaste da superfície do trilho e grau de amarração."
+  ],
+  images: [
+    'assets/images/rail_geometry/1.png',
+    'assets/images/rail_geometry/2.png',
+    'assets/images/rail_geometry/3.png',
+    'assets/images/rail_geometry/4.png',
   ]
 };
 
